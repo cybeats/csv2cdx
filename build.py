3b7ad8e7b5f5c955594cd4beea71d3d2b39e05a7
Richard_E <restwick@cybeats.com>
2023-04-20 18:10:18 -0400

    [pbs] added detail to sbom generation

--- build.py
+++ build.py
@@ -35,61 +35,68 @@ class Builder2:
         
    
     def get_hashes(self, hashes, csv_data) -> list:
-        hash_list = []
-        try:
-            for hash in hashes:
-                alg = self.get_hash_algo(csv_data.get(hash.get("hash_alg", None)))
-                hash_result = HashType(
-                                        alg=alg,
-                                        content=csv_data(hash.get("hash_content", None))
-                                    )
-            hash_list.append(hash_result)
-        except:
-            #hash_list = None
-            pass
-        return hash_list
+        hashes = [self.get_hash(hash, csv_data) for hash in hashes]
+        hashes = [i for i in hashes if i is not None]
+        return hashes
+
+    def get_hash(self, hash, csv_data) -> list:   
+
+        alg = self.get_hash_algo(csv_data.get(hash.get("hash_alg")))
+        content=csv_data.get(hash.get("hash_content"))
+        hash_result = HashType(
+                                alg=alg,
+                                content=content
+                            )
+    
+        return hash_result
     
     
 
     
     def get_licenses(self, licenses, csv_data) -> list:
-        license_list = []
-        try:
-            for license in licenses:
-                url = self.build_url(csv_data.get(license.get("license_url")))
-                id=csv_data.get(license.get("license_id"))
-                name=csv_data.get(license.get("license_name"))
-                lic_obj = License(
-                                    id=id, 
-                                    name=name, 
-                                    url=url
-                                )
-
-                lic = LicenseChoice(license=lic_obj)
-                license_list.append()
-        except:
-            #license_list = None
-            pass
-        return license_list
+        licenses = [self.get_license(license, csv_data) for license in licenses]
+        licenses = [i for i in licenses if i is not None]
+        return licenses 
     
 
-    def get_exRef(self, references, csv_data) ->list:
-        exref_list = []
-        try:
-            for exref in exref_list:
-                type = ExternalReferenceType(csv_data.get(exref.get("er_type")))
-                url = self.build_url(csv_data(exref.get("er_url")))
-                exref_data = ExternalReference(
-                                                type=type, 
-                                                url=url
-                                            )
-                
-                exref_list.append(exref_data)
-        except:
-            #exref_list = None
-            pass
-        return exref_list
+    def get_license(self, license, csv_data):
+        url =csv_data.get(license.get("license_url"))
+        id=csv_data.get(license.get("license_id"))
+        name=csv_data.get(license.get("license_name"))
+        if url is not None:
+            url = XsUri(url)
+        if (name is None) and (id is None):
+            return
+        
+        license = License(  
+                            url=url,
+                            name=name,
+                            id=id
+                        )
+        license_choice = LicenseChoice( 
+                                        license=license
+                                    )
+        return license_choice
 
+    def get_exRefs(self, references, csv_data) ->list:
+        exRefs = [self.get_exRef(reference, csv_data) for reference in references]
+        exRefs = [i for i in exRefs if i is not None]
+        return exRefs 
+    
+    
+    def get_exRef(self, reference, csv_data) ->ExternalReference:
+        type_ref = csv_data.get(reference.get("er_type"))
+        url = csv_data.get(reference.get("er_url"))
+        if type_ref is None:
+            return None
+        type_ref = ExternalReferenceType(type_ref)
+        url = XsUri(url)
+        exref_data = ExternalReference(
+                                        type=type_ref, 
+                                        url=url
+                                    )
+        return exref_data
+    
      
     
     def build_contacts(self, name, email, phone) -> OrganizationalContact:
@@ -120,12 +127,11 @@ class Builder2:
 
     
     def build_url(self, url) -> XsUri:
+        s_url = None
         try:
             s_url = XsUri(url)
         except:
-
-            #s_url = None
-            pass
+            s_url = None
         return s_url
     
 
@@ -149,7 +155,7 @@ class Builder2:
 
         percentage = ((iteration + 1) / total * 100)
         print("\rgetting components-{}%".format(int(percentage)), sep=' ', end='', flush=True)
-        
+    
         name = config_data.get("name")
         version = config_data.get("version")
         type = config_data.get("type")
@@ -177,14 +183,14 @@ class Builder2:
         bom_ref = csv_data.get(bom_ref)
         group = csv_data.get(group)
         publisher = csv_data.get(publisher)
+
+
         purl = PackageURL.from_string(csv_data.get(purl))
-        #purl=csv_data.get(purl)
+
+
         licenses = self.get_licenses(licenses, csv_data)
-        
-
         hashes = self.get_hashes(hashes, csv_data)
-
-        externalReferences = self.get_exRef(externalReferences, csv_data)
+        externalReferences = self.get_exRefs(externalReferences, csv_data)
 
         mime_type = csv_data.get(mime_type)
         description = csv_data.get(description)
@@ -225,9 +231,6 @@ class Builder2:
         return sbom_component
 
             
-
-    
-
     def assemble_components(self, csv_data, config_data) -> list:
         config_data=config_data.get("component_configuration")
         total = len(csv_data)
@@ -276,8 +279,10 @@ class Builder2:
         metadata_component = Component(
                                         name=setup_data.get("sbom_name"), 
                                         version=setup_data.get("sbom_version"),
-                                        type=ComponentType( setup_data.get("sbom_type")),
-                                        namespace= setup_data.get("namespace")                                                                               
+                                        type=ComponentType(setup_data.get("sbom_type")),
+                                        namespace= setup_data.get("namespace"),
+                                        purl=self.make_purl(setup_data.get("package_type"), setup_data.get("sbom_name"),setup_data.get("sbom_version")),
+
                                     )
         #metadata_component = None
 
@@ -309,208 +314,11 @@ class Builder2:
         try:
             outputter.output_to_file(self.ouput_file)
         except FileExistsError:
-            answer = input("this sbom already exists. do you want to overwrite? (Y/N):   ")
-            if answer == "y" or "Y":
+            answer = input("this sbom file already exists. do you want to overwrite? (Y/N):   ")
+            if answer == ("y" or "Y"):
                 print("overwriting file...")
                 os.remove(self.ouput_file)
                 outputter.output_to_file(self.ouput_file)
+            else:
+                print("ok then")
         print("finished. have a nice day!")
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
-
-
-class Builder:
-
-    def __init__(self, csv_data :dict, json_data :dict) -> None:
-
-        self.name = None
-        self.version = None
-        self.package = None
-
-        self.supplier = None
-        self.supplier_urls = None
-        self.supplier_contact = None
-        self.supplier_contact_name = None
-        self.supplier_contact_phone = None
-        self.supplier_contact_email = None
-        self.comps = []
-
-        self.purl = None
-        self.swid = None
-        self.cpe = None
-        self.type = None
-
-        self.values = csv_data
-        self.config = json_data
-        self.control = self.config.get("configuration")
-        self.manufacturer = {}
-
-        self.bom =  Bom()
-
-        self.check = [
-                        "application", 
-                        "framework", 
-                        "library", 
-                        "container", 
-                        "operating-system", 
-                        "device", 
-                        "firmware", 
-                        "file"
-                    ]
-
-    def if_exist(self, array, arg):
-        if arg in array:
-            return True
-        else:
-            return False
-
-    def get_component_param(self,array, parameter):
-        if self.if_exist(array, parameter) == False:
-            return None
-        pass
-
-    def get_config_param(self, array, parameter):
-        if self.if_exist(array, parameter) == False:
-            return None
-        pass
-
-    def check_in(self, term :str, assay :dict):
-        if term not in assay:
-            print("WARNING {} not present".format(term))
-
-    
-    def cpe_wildcard(cpe :str):
-        cpe = cpe.split(":")
-        if "cpe" != cpe[0]:
-            return None 
-        cpe[-1] = ".*.*.*.*.*.*"
-        return cpe.join() 
-
-
-    def make_purl(self, package, name, version):
-        if package == None:
-            package = 'generic'
-        purl = "pkg:{}/{}@{}".format(package, name, version)
-        purl_formatted  = PackageURL.from_string(purl)
-        return purl_formatted
-
-
-    
-
-
-    def build_components(self, val):
-
-        for key in val:
-            name = val[key].get(self.control.get("name"))
-            version = val[key].get(self.control.get("version"))
-
-            if "supplier_urls" in val[key]:
-                
-                self.supplier_urls = XsUri(val[key].get("supplier_urls"))
-
-           
-           
-            if "supplier_contact_name" in val[key]:
-                contacts = OrganizationalContact(
-                                                    name=val[key].get("supplier_name", None), 
-                                                    email=val[key].get("supplier_contact_email", None), 
-                                                    phone=val[key].get("supplier_contact_phone", None)
-                                                )
-            else:
-                contacts = None
-            
-            if "supplier_contact_name" in val[key]:           
-                self.supplier = OrganizationalEntity(
-                                                        name=val[key].get("supplier_name", None), 
-                                                        urls=  self.supplier_urls, 
-                                                        contacts= contacts
-                                                    )
-            else:
-                self.supplier = None      
-        
-
-            self.cpe = val[key].get(self.control.get("cpe"))
-
-
-            name = val[key].get(self.control.get("name"))
-            version = val[key].get(self.control.get("version"))
-            
-            if self.config.get('add_purl')  is True:
-                self.purl = self.make_purl(self.config.get("package_type"), name, version) 
-
-            
-            if self.config.get('wildcard') == "true":
-                self.cpe = self.cpe_wildcard(self.cpe) 
-
-            self.type=ComponentType(val[key].get(self.control.get('type')))
-
-            self.comps.append(
-                                Component(
-                                            name= name,       
-                                            version=version, 
-                                            purl=self.purl, 
-                                            type=self.type, 
-                                            supplier=self.supplier, 
-                                            swid=val[key].get(self.control.get("swid", None)), 
-                                            cpe=self.cpe
-
-                                        )
-                            )
-
-            
-   
-   
-   
-    def create_metadata(self, config:dict):
-
-        if "type" not in config:
-            print("WARNING, NO TYPE GIVEN")  
-
-        if config["type"] not in self.check:
-            print("ERROR, UNKNOWN TYPE")
-            exit()
-
-        version = config.get("sbom_component_version", None)
-
-        if config.get("manufacturer_name", None) is not None:
-            self.bom.metadata.manufacture = OrganizationalEntity(
-                                                    name=config.get("manufacturer_name", None),
-                                                    urls= config.get("manufacturer_url", None), 
-                                                    contacts= config.get("manufacturer_contact", None)
-                                               )
-        
-        self.bom.metadata.component = Component(
-                                                    name=config.get("sbom_component_name", None), 
-                                                    type=ComponentType(config.get("sbom_type", None)),
-                                                    namespace= config.get("namespace", None),
-                                                    version= version                                                   
-                                                )   
-    
-    
-    def create_sbom(self):
-        self.build_components(self.values)
-        self.create_metadata(self.config)
-        self.bom.components=self.comps
-        outputter: BaseOutput = get_instance(bom=self.bom, output_format=OutputFormat.JSON)
-        bom_json: str = outputter.output_as_string()
-        parsed = json.loads(bom_json)
-        newbom = json.dumps(parsed, indent=4)
-        with open("sbom.json", "w") as nb:
-            nb.write(newbom)
-  
-
-
