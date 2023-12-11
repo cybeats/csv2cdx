eccbcd923c0ebf77a2fedc26356f92a0b7231bc7
Richard Estwick <richard@cybeats,com>
2023-12-10 23:42:54 -0500

    updated lib, update tests

--- csv2cdx/__main__.py
+++ csv2cdx/__main__.py
@@ -1,15 +1,16 @@
 import csv2cdx
 from .parse import Parser
-from .build import Builder2
+from .build import Builder
 
 def main():
     parser = Parser()
 
     args, data, config = parser.get_data()
 
-    builder = Builder2(args, data, config)
+    builder = Builder(args, data, config)
 
     builder.build_sbom()
+
 
 if __name__ == "__main__":
     main()
